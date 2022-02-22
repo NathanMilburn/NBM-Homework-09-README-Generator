ee823578548a40fe7cce9f2ac8cd5e850b23ad2c
NathanM <nathan.milburn@outlook.com>
2022-02-21 19:37:12 -0800

    installed the prettier extenstion to clean up my js code

--- index.js
+++ index.js
@@ -1,130 +1,220 @@
 // Nodejs modules used
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // Inquirer prompts
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your project title?',
-            name: 'title',
-            // Property validation to ensure user has submitted relevant information for their README.md file
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What is your user story? This follows traditional "AS A, I WANT, SO THAT" formatting. AS A: ',
-            name: 'userStory',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What is your user story? This follows traditional "AS A, I WANT, SO THAT" formatting. I WANT: ',
-            name: 'userStory2',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What is your user story? This follows traditional "AS A, I WANT, SO THAT" formatting. SO THAT: ',
-            name: 'userStory3',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What is the description for this project?',
-            name: 'description',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What was your steps process for this project?',
-            name: 'process',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What are the use cases for your app?',
-            name: 'usage',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'How do you use your application?',
-            name: 'howTo',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What are your future development plans?',
-            name: 'futureDevelopments',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What were your biggest challenges for this project?',
-            name: 'challenges',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What are the links to your deployed repository and application?',
-            name: 'deployedLinks',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'github',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'Please link your LinkedIn account.',
-            name: 'linkedin',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'Please enter your email address.',
-            name: 'email',
-            validate: (value)=>{if(value){return true} else{return 'A value is needed to continue'}}
-        },
-    ])
-    .then(({
-        title,
-        userStory,
-        userStory2,
-        userStory3,
-        description,
-        process,
-        usage,
-        howTo,
-        futureDevelopments,
-        challenges,
-        deployedLinks,
-        github,
-        linkedin,
-        email
+  .prompt([
+    {
+      type: "input",
+      message: "What is your project title?",
+      name: "title",
+      // Property validation to ensure user has submitted relevant information for their README.md file
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message:
+        'What is your user story? This follows traditional "AS A, I WANT, SO THAT" formatting. AS A: ',
+      name: "userStory",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message:
+        'What is your user story? This follows traditional "AS A, I WANT, SO THAT" formatting. I WANT: ',
+      name: "userStory2",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message:
+        'What is your user story? This follows traditional "AS A, I WANT, SO THAT" formatting. SO THAT: ',
+      name: "userStory3",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What is the description for this project?",
+      name: "description",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What was your installation steps process for this project?",
+      name: "installation",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What are the use cases for your app?",
+      name: "usage",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "How do you use your application?",
+      name: "howTo",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What are your future development plans?",
+      name: "futureDevelopments",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What were your biggest challenges for this project?",
+      name: "challenges",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message:
+        "What are the links to your deployed repository and application?",
+      name: "deployedLinks",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "Please link your LinkedIn account.",
+      name: "linkedin",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "Please enter your email address.",
+      name: "email",
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return "A value is needed to continue";
+        }
+      },
+    },
+  ])
+  .then(
+    ({
+      title,
+      userStory,
+      userStory2,
+      userStory3,
+      description,
+      installation,
+      usage,
+      howTo,
+      futureDevelopments,
+      challenges,
+      deployedLinks,
+      github,
+      linkedin,
+      email,
     }) => {
-    const template = `# ${title}
+      const template = `# ${title}
+## DESCRIPTION
+${description}
+
 ## TABLE OF CONTENTS
-- [UserStory](#userStory)
-- [PROCESS](#process)
-- [Usage](#usage)
-- [ContactInfo](#github)
-- [License](#license)
+- [USERSTORY](#userStory)
+- [INSTALLATION](#installation)
+- [USAGE](#usage)
+- [LICENSE](#license)
+- [CONTACT](#contact)
 
 ## USER STORY
 AS A ${userStory}
 I WANT ${userStory2}
 SO THAT ${userStory3}
 
-## DESCRIPTION
-${description}
 
-## PROCESS
-${process}
+## INSTALLATION
+${installation}
     
 ## USE CASES
 ${usage}
@@ -164,28 +254,32 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-## CONTACT INFO
+## CONTACT
 * GitHub: ${github}
 * LinkedIn: ${linkedin}
 * E-Mail: ${email}`;
 
-createNewReadMe(title, template);
-}
-);
-function createNewReadMe(fileName, data){
-fs.writeFile(`./${fileName.toUpperCase().split(' ').join('')}.md`, data, (err) => {
-    if(err){
-        console.log(err)
+      createNewReadMe(title, template);
     }
-    console.log('README.md File Created!');
-})
+  );
+function createNewReadMe(fileName, data) {
+  fs.writeFile(
+    `./${fileName.toUpperCase().split(" ").join("")}.md`,
+    data,
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("README.md File Created!");
+    }
+  );
 }
 
-    // Previously developed code based on index.html generator from previous class mini project.
-    // .then((answers) => {
-    //     const generateREADMEPage = generateHTML(answers)
+// Previously developed code based on index.html generator from previous class mini project.
+// .then((answers) => {
+//     const generateREADMEPage = generateHTML(answers)
 
-    //     fs.writeFile('README.md', generateREADMEPage, (err) => 
-    //     err ? console.log(err) : console.log('README.md File Created!')
-    //     );
-    // });
+//     fs.writeFile('README.md', generateREADMEPage, (err) =>
+//     err ? console.log(err) : console.log('README.md File Created!')
+//     );
+// });
